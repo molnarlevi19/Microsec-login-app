0d22fcc5031ea44e5f33026d3e8ee98bafe5b790
molnarlevi19 <molnarlevi19@gmail.com>
2024-10-02 19:45:58 +0200

    chore: initialize frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Landing from './pages/Landing';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/landing" element={<Landing />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App
